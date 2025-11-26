854dbc08eff1e365b04daca3de0d8f4c3cce05e2
Amine Abbes <amine.abbes94370@gmail.com>
2025-11-26 10:22:33 +0100

    ajout de TenAccount.php

---
+++ src/TeenAccount.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Abbesamine\MyWeeklyAllowance;
+
+final class TeenAccount
+{
+    private string $name;
+    private float $balance;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+        $this->balance = 0.0;
+    }
+
+    public function getBalance(): float
+    {
+        return $this->balance;
+    }
+
+    public function deposit(float $amount): void
+    {
+        if ($amount <= 0) {
+            throw new \InvalidArgumentException('Deposit amount must be positive.');
+        }
+
+        $this->balance += $amount;
+    }
+}
